6aa11e1b9f7802a351b21022e25a2224907e9461
Gianluca Scopelliti <gianlu.1033@gmail.com>
2019-11-25 19:17:10 +0100

    testing timer

--- project/sleep.ino
+++ project/sleep.ino
@@ -1,9 +1,10 @@
 //TODO interrupt handlers, pins, flags ...
-int wake_pin = 3; // select between 0, 1, 2, 3, 7
-int system_clock = 16e6;
+#define WAKE_PIN 3 // select between 0, 1, 2, 3, 7
+#define SYS_CLK 16e6
+#define CNT_CLK SYS_CLK / 1024
 
 void setup_sleep() {
-  pinMode(wake_pin, INPUT_PULLUP); // select between INPUT and INPUT_PULLUP
+  pinMode(WAKE_PIN, INPUT_PULLUP); // select between INPUT and INPUT_PULLUP
 }
 
 void wakeUp() {
@@ -12,20 +13,43 @@ void wakeUp() {
 
 }
 
-void enterIdleMode(int seconds) {
-  //TODO: set timer
+// TCCR3A
+// 7       6       5      4      3     2      1     0
+//COM3A1 COM3A0 COM3B1 COM3B0 COM3C1 COM3C0 WGM31 WGM30
+// TCCR3B
+// 7    6    5   4      3    2     1    0
+//CNC3 ICES3 – WGM33 WGM32 CS32 CS31 CS30
 
+void enterIdleMode(int seconds) {
+  //set the counter
+  //TODO now i will set one second
+  OCR3A = CNT_CLK;
+  
+  //set clock source = clk / 8 -> write 010 to CS32..CS30 for /8, write 101 for /1024
+  //set mode 7 (up to OCR3A)
+  //TODO change only the bits i'm interested in
+  TCCR3A = 0;
+  TCCR3B = 0b00001101;
+
+  //enable interrupt
+  TIMSK3 |= 0b00000010;
+  
   //SleepMode.idle(...);
+}
+
+ISR (TIMER3_COMPA_vect)
+{
+  Serial.println("Interrupt!");
 }
 
 void enterPowerDownMode() {
     Serial.println("enteredF");
-    attachInterrupt(digitalPinToInterrupt(wake_pin), wakeUp, CHANGE); // select between CHANGE, LOW, RISING, FALLING
+    attachInterrupt(digitalPinToInterrupt(WAKE_PIN), wakeUp, CHANGE); // select between CHANGE, LOW, RISING, FALLING
     
     Serial.println("Entering low power mode");
     delay(20);
     SleepMode.powerDown(ADC_OFF, BOD_OFF);
     Serial.println("External interrupt received, waking up from low power mode");
     
-    detachInterrupt(digitalPinToInterrupt(wake_pin));
+    detachInterrupt(digitalPinToInterrupt(WAKE_PIN));
 }
